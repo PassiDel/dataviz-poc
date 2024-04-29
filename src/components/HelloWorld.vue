51e8a16dde53b335db50b9c526b67dd449ee13b7
Pascal Syma <pascal@syma.dev>
2024-04-29 17:59:37 +0200

    Add tailwind and nvmrc

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,8 +5,12 @@ defineProps<{
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+  <div class="">
+    <h1
+      class="m-3 rounded-3xl bg-red-700 p-4 text-3xl transition hover:bg-green-700"
+    >
+      {{ msg }}
+    </h1>
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
@@ -16,27 +20,4 @@ defineProps<{
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
+<style scoped></style>
