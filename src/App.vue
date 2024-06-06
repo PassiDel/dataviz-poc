0d4dea2ea65da8053890bfa4ced8714a403e520f
Pascal Syma <pascal@syma.dev>
2024-06-06 15:30:11 +0200

    Logo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
+import logo from '@/assets/logo_hsb_startseite.svg';
 </script>
 
 <template>
   <VaLayout :top="{ fixed: true, order: 2 }" class="h-dvh">
     <template #top>
-      <VaNavbar color="secondary">
+      <VaNavbar class="test">
         <template #left
-          ><RouterLink to="/">Die Hochschule in Zahlen!</RouterLink></template
+          ><RouterLink to="/" class="flex flex-row text-white"
+            ><img :src="logo" class="h-10" alt="Logo HSB" /><span
+              class="my-auto"
+              >Die Hochschule in Zahlen!</span
+            ></RouterLink
+          ></template
         >
         <template #right>
-          <div class="flex flex-col gap-2 md:flex-row md:gap-4">
+          <div class="flex flex-col gap-2 text-white md:flex-row md:gap-4">
             <RouterLink to="/" activeClass="font-bold" class="hover:underline"
               >Start</RouterLink
             >
@@ -36,4 +42,8 @@ import { RouterLink, RouterView } from 'vue-router';
   </VaLayout>
 </template>
 
-<style scoped></style>
+<style scoped>
+.test {
+  background: linear-gradient(to right, rgb(10, 85, 140), rgb(50, 180, 200));
+}
+</style>
