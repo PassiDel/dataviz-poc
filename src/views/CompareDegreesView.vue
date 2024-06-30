219d9f4f1d4581d82266488fd505ac6ba8a5cbe7
Pascal Syma <pascal@syma.dev>
2024-06-30 13:14:03 +0200

    Group scrolling on compare page

--- src/views/CompareDegreesView.vue
+++ src/views/CompareDegreesView.vue
@@ -2,11 +2,11 @@
 import DataSelect from '@/components/DataSelect.vue';
 import { computed, ref } from 'vue';
 import { degrees, faculties, type Semester, sumDegreesAllYears } from '@/data';
-import CompareDegree from '@/components/CompareDegree.vue';
 import { useQuery } from '@/composables/useQuery';
-import CompareFaculty from '@/components/CompareFaculty.vue';
-import PlayBar from '@/components/PlayBar.vue';
 import { getSemesterFromDegrees } from '@/utils/semester';
+import CompareFaculty from '@/components/CompareFaculty.vue';
+import CompareDegree from '@/components/CompareDegree.vue';
+import PlayBar from '@/components/PlayBar.vue';
 
 const { left, right } = useQuery({ left: -1, right: -1 });
 
@@ -48,67 +48,73 @@ const year = ref<Semester>(years.value[years.value.length - 1]);
 </script>
 
 <template>
-  <main class="flex grid-cols-6 flex-col gap-3 p-3 pb-24 md:grid">
+  <main
+    class="flex grid-cols-6 flex-col gap-3 pb-24 pt-0 md:grid md:h-full md:pb-3"
+  >
     <DataSelect
       v-model:selected="left"
       :selectedFaculty="leftDegree?.faculty"
     />
-    <CompareDegree
-      v-if="leftDegree"
-      :degree="leftDegree"
-      :year="year"
-      class="degree"
-    />
-    <CompareFaculty
-      v-else-if="leftFaculty"
-      :faculty="leftFaculty"
-      :year="year"
-      class="degree"
-    />
-    <CompareDegree
-      v-else-if="left === -1"
-      :year="year"
-      :degree="hsb"
-      class="degree"
-    />
-    <div v-else class="col-span-2">
-      <h2 class="hidden md:block">
-        Wähle links einen <span class="font-bold">Studiengang</span><br />
-        oder eine <span class="font-bold">Fakultät</span> aus!
-      </h2>
-      <h2 class="block md:hidden">
-        Wähle oben einen <span class="font-bold">Studiengang</span><br />
-        oder eine <span class="font-bold">Fakultät</span> aus!
-      </h2>
-    </div>
-    <hr class="my-4 border-2 border-primary md:hidden" />
-    <CompareDegree
-      v-if="rightDegree"
-      :degree="rightDegree"
-      :year="year"
-      class="degree"
-    />
-    <CompareFaculty
-      v-else-if="rightFaculty"
-      :faculty="rightFaculty"
-      :year="year"
-      class="degree"
-    />
-    <CompareDegree
-      v-else-if="right === -1"
-      :year="year"
-      :degree="hsb"
-      class="degree"
-    />
-    <div v-else class="col-span-2">
-      <h2 class="hidden md:block">
-        Wähle rechts einen <span class="font-bold">Studiengang</span><br />
-        oder eine <span class="font-bold">Fakultät</span> aus!
-      </h2>
-      <h2 class="block md:hidden">
-        Wähle unten einen <span class="font-bold">Studiengang</span><br />
-        oder eine <span class="font-bold">Fakultät</span> aus!
-      </h2>
+    <div
+      class="col-span-4 flex grid-cols-4 flex-col gap-3 overflow-y-auto pt-3 md:grid md:h-full md:pb-24"
+    >
+      <CompareDegree
+        v-if="leftDegree"
+        :degree="leftDegree"
+        :year="year"
+        class="degree"
+      />
+      <CompareFaculty
+        v-else-if="leftFaculty"
+        :faculty="leftFaculty"
+        :year="year"
+        class="degree"
+      />
+      <CompareDegree
+        v-else-if="left === -1"
+        :year="year"
+        :degree="hsb"
+        class="degree"
+      />
+      <div v-else class="col-span-2">
+        <h2 class="hidden md:block">
+          Wähle links einen <span class="font-bold">Studiengang</span><br />
+          oder eine <span class="font-bold">Fakultät</span> aus!
+        </h2>
+        <h2 class="block md:hidden">
+          Wähle oben einen <span class="font-bold">Studiengang</span><br />
+          oder eine <span class="font-bold">Fakultät</span> aus!
+        </h2>
+      </div>
+      <hr class="my-4 border-2 border-primary md:hidden" />
+      <CompareDegree
+        v-if="rightDegree"
+        :degree="rightDegree"
+        :year="year"
+        class="degree"
+      />
+      <CompareFaculty
+        v-else-if="rightFaculty"
+        :faculty="rightFaculty"
+        :year="year"
+        class="degree"
+      />
+      <CompareDegree
+        v-else-if="right === -1"
+        :year="year"
+        :degree="hsb"
+        class="degree"
+      />
+      <div v-else class="col-span-2">
+        <h2 class="hidden md:block">
+          Wähle rechts einen <span class="font-bold">Studiengang</span><br />
+          oder eine <span class="font-bold">Fakultät</span> aus!
+        </h2>
+        <h2 class="block md:hidden">
+          Wähle unten einen <span class="font-bold">Studiengang</span><br />
+          oder eine <span class="font-bold">Fakultät</span> aus!
+        </h2>
+      </div>
     </div>
     <DataSelect
       v-model:selected="right"
